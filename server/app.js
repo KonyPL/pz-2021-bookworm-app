5124d89eeb88d6a127f4599771ab56690830725f
KonyPL <mrkony98@gmail.com>
2021-05-26 01:48:42 +0200

    Added reviews routing

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
 var genresRouter = require('./routes/genres');
 //var userBookRouter = require('./routes/userBooks');
-//var reviewsRouter = require('./routes/reviews');
+var reviewsRouter = require('./routes/reviews');
 var genreRequestsRouter = require('./routes/genreRequests');
 
 
@@ -53,7 +53,7 @@ app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/books/genres', genresRouter);
 //app.use('/user-books', userBookRouter);
-//app.use('/reviews', reviewsRouter);
+app.use('/reviews', reviewsRouter);
 app.use('/books/genres/requests', genreRequestsRouter);
 
 
---
+++ server/models/review.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ReviewSchema = new Schema(
+  {
+    user_id: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    book_id: {type: Schema.Types.ObjectId, ref: 'Book', required: true},
+    review_rating: {type: Number, default: 0, min: 0, max: 10, required: true},
+    review_content: {type: String, required: false}
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Review', ReviewSchema);
